6b732885753bee1d047e503f14aee434aec76452
sebastien <seb17-2@hotmail.fr>
2019-12-12 11:29:39 +0100

    Add the MDB React package

---
+++ src/components/log_component/login/Login.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <main className = "main">
+
+      </main>
+    );
+  }
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router , Switch, Route } from "react-router-dom";
+import Login from "./components/log_component/login";
+import SignUp from "./components/log_component/signup";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Import the MDBReact's requirement
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+const routing = (
+    <Router>
+      <div id="routing-container">
+        <Switch>
+          <Route path="/" exact component={Login}></Route>
+          <Route path="/signin" component={SignUp}></Route>
+        </Switch>
+      </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
