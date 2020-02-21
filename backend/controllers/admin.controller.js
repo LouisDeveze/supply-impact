0fb10b07b61dfbf6e0f26d2a080f3af52136b7b1
unknown <helene.carlier.gubler@gmail.com>
2020-02-21 12:01:16 +0100

    findOne pas test

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -67,3 +67,106 @@ exports.create = (req, res) => {
       });
     });
 };
+
+// Find a single Admin with an email
+exports.findOne = (req, res) => {
+
+  //If id is pass in request
+  if(req.body){
+    var diffParams = {};
+
+    if(req.body.email){
+      diffParams.email = req.body.email;
+      console.log("Email : " + diffParams.email);
+    }else{
+      return res.status(404).send({
+        message: 'No email in the body for Admin findOne'
+      });
+    }
+
+    //on ne demande pas le password
+    Admin.find(diffParams, {password : 0})
+    .then(admin => {
+      if (!admin) {
+        return res.status(404).send({
+          message: 'Admin not found with thoses : ' + diffParams
+        });
+      }
+      
+      res.send(user[0]);
+    })
+    .catch(err => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: 'Admin not found with thoses params ' +diffParams
+        });
+      }
+      return res.status(500).send({
+        message: 'Error retrieving admin with thoses params' + diffParams
+      });
+    });
+  }
+  else{
+    return res.status(404).send({
+      message: 'No params in request'
+    });
+  }
+};
+
+/*
+
+// Update an Admin identified by the AdminId in the request
+exports.update = (req, res) => {
+  // Validate Request
+  if (!req.body.adminId) {
+    return res.status(400).send({
+      message: 'adminId can not be empty'
+    });
+  }
+
+  admin.findById(req.body.userId).lean()
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({
+          message: 'User not found with id ' + req.body.userId
+        });
+      }else{
+        const userReceived = req.body;
+        const newUser = Object.assign({}, user, userReceived);
+        delete newUser.userId;
+        newUser.personsInHouse = Number(newUser.personsInHouse);
+        
+        // Find user and update it with the request body
+        User.findByIdAndUpdate(
+          req.body.userId,
+          {$set: {
+            location: newUser.location,
+            personsInHouse: newUser.personsInHouse,
+            houseSize: newUser.houseSize
+          }},
+          { new: true }
+        )
+          .then(userMod => {
+            if (!userMod) {
+              return res.status(404).send({
+                message: 'User not found with id ' + req.body.userId
+              });
+            }
+            res.send(userMod);
+          })
+          .catch(err => {
+            if (err.kind === 'ObjectId') {
+              return res.status(404).send({
+                message: 'User not found with id ' + req.body.userId
+              });
+            }
+            return res.status(500).send({
+              message: 'Error updating user with id ' + req.body.userId
+            });
+          });
+      }
+        
+  })
+};
+
+*/
