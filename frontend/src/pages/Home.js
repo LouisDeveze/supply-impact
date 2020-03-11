ba4864472cab6b9109782b1acd5e682d149e4e65
adb <alexdebeaudrap@gmail.com>
2020-03-11 16:14:05 +0100

    home page 1st

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import NavHome from "../components/nav/NavHome";
-import { MDBBox, MDBContainer } from 'mdbreact';
+import { MDBBox, MDBContainer,MDBCard,MDBCardTitle,MDBCardText,MDBRow,MDBCol } from 'mdbreact';
 import "./pages.css"
 
 export default class Home extends Component {
@@ -16,34 +16,76 @@ export default class Home extends Component {
     
     render() {
         return (
-            <body className="Homed" style={{ paddingTop:"0px", paddingBottom:"0px"}}>
+            <div className="Home" style={{width: "100%", height:"100%", paddingTop:"0px", paddingBottom:"0px"}}>
+                <div className="site-content">
+                    <NavHome />
+
+                    <h1 className="global_title_1">Welcome on Supply'Impact {this.data.name} !</h1>
+
+                    <MDBContainer fluid  style={{ height:"100%"}} >
+                        <MDBRow >
+                            <MDBCol sm="6">
+                                <MDBRow >
+                                    <MDBCol sm="6">
+                                            <MDBCard className="card-body" style={{ width: "100%", marginTop: "1rem" }}>
+                                            <MDBCardTitle>Nombre de Fournisseurs</MDBCardTitle>
+                                            <MDBCardText>
+                                                {this.data.nbFourn}
+                                            </MDBCardText>
+                                        </MDBCard>
+                                    </MDBCol>
+
+                                    <MDBCol sm="6">
+                                        <MDBCard className="card-body" style={{ width: "100%", marginTop: "1rem" }}>
+                                            <MDBCardTitle>Nombre de Produits</MDBCardTitle>
+                                            <MDBCardText>
+                                                {this.data.nbProd}
+                                            </MDBCardText>
+                                        </MDBCard>
+                                    </MDBCol>
+                                </MDBRow>
+                                <MDBRow >
+
+                                <MDBCol sm="12">
+                                    <MDBCard className="card-body" style={{ width: "100%", height:"100%", marginTop: "1rem" }}>
+                                        <MDBCardTitle>camembert stat produits</MDBCardTitle>
+                                        <MDBCardText>
+                                            flexf
+                                            
+                                            <p>d</p>
+                                            <p>d</p>
+                                        </MDBCardText>
+                                    </MDBCard>
+                                </MDBCol>
+                                    
+                                </MDBRow>
+                            </MDBCol>
+
+                            <MDBCol sm="6">
+                                <MDBCard className="card-body" style={{ width: "100%", height:"100%", marginTop: "1rem" }}>
+                                    <MDBCardTitle>caroussel dernier produits add</MDBCardTitle>
+                                    <MDBCardText>
+                                        caroussel
+                                            <p>d</p>
+                                            <p>d</p>
+                                            <p>d</p>
+                                            <p>d</p>
+                                            <p>d</p>
+                                            <p>d</p>
+                                            <p>d</p>
+                                    </MDBCardText>
+                                </MDBCard>
+
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBContainer>
+                </div>
                 
-                <NavHome />
-
-                <MDBContainer fluid>
-                        Content here -->
-                </MDBContainer>
-                <h1 className="global_title_1">Welcome on Supply'Impact {this.data.name} !</h1>
-
-                <MDBBox display="inline" >
-                    <MDBBox display="block">
-                        <MDBBox display="inline" bgColor="primary">
-                            nombre de fournisseur : {this.data.nbFourn}
-                        </MDBBox>
-                        <MDBBox display="inline">
-                            nombre de produits : {this.data.nbProd}
-                        </MDBBox>
-                    </MDBBox>
-                    <MDBBox display="block" color="red">
-                        camembert stat produits
-                    </MDBBox>
-                </MDBBox>
-                <MDBBox display="inline">
-                    caroussel dernier produits add
-                </MDBBox>
 
                 
-            </body>
+               
+
+            </div>
         );
     }
 }
